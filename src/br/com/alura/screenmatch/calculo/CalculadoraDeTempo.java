847dad620c16c9de29bd1094dc8c9e767dc735fd
lucas <dev.lucas77@gmail.com>
2026-09-23 18:15:21 -0300

    Aplicação de polimorfismo, criação do pacote e classe CalculadoraDeTempo para armazenar titulo(seja serie ou filme) calcular a duração de tempo.

---
+++ src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.calculo;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal(){
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme f){
+//        tempoTotal = tempoTotal + f.getDuracaoEmMinutos();
+//    }
+//    public void inclui(Serie s){
+//        tempoTotal = tempoTotal + s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de: " + titulo.getNome());
+        tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
